bcc96ddae7024adabe16f27c2e9bf07c4d0e22b4
Alwy Christyo <alwychristyo@gmail.com>
2018-12-14 14:17:32 +1100

    add timeout on clicks

--- src/spec/library/logout/index.js
+++ src/spec/library/logout/index.js
@@ -1,16 +1,14 @@
 import { SECONDS } from 'constants';
+import { setDefaultOptions } from 'expect-puppeteer'
 
+setDefaultOptions({ timeout: 25 * SECONDS })
 class Logout {
   run(page) {
     return new Promise(async (resolve, reject) => {
       try {
-        // login.run("Some Url");
-        console.log("Logout triggered");
-        await page.waitFor(20 * SECONDS);
-
-        // dropdown testid  button-i3smb2jf
+        // Click on user dropdown (usually top right corner)
         await page.click('[data-testid~="USER_DROPDOWN"]');
-        // logout button test id
+        // Click on the logout button
         await page.click('a[href="/logout"]');
         resolve(true);
       } catch (error) {
