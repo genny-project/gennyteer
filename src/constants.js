fc80c968e7dcddb6aafc20c6bce74954588ec9eb
Alwy Christyo <alwychristyo@gmail.com>
2018-12-18 10:26:07 +1100

    Changed the default resolution of the browser. Supress the VNC logs a bit (-q)

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ import UUID from 'short-uuid';
 
 export const JOB_ID = `TEST_LOAD_${( new UUID ).new()}`;
 export const TEST_TIMEOUT = 10000;
-export const PAGE_WIDTH = 1920;
-export const PAGE_HEIGHT = 1080;
+export const PAGE_WIDTH = 1600;
+export const PAGE_HEIGHT = 900;
 export const SECONDS = 1000;
