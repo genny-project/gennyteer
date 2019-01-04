f6bbe27235a54012fd37a0be2c17090441b3eb21
c7yrus <c7yrus@gmail.com>
2019-01-04 13:10:57 +1100

    updated the structure for the test

--- src/spec/library/index.js
+++ src/spec/library/index.js
@@ -1,11 +1,12 @@
 import Login from './login';
 import Logout from './logout';
 import Register from './register';
-
+import {GennyTest} from './libutils';
 
 export {
   Login,
   Logout,
-  Register
+  Register,
+  GennyTest
 };
 
