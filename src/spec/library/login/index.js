082ff4f544613e22f5f17eaad7eb40700d859963
Nischal Gautam <nischal1024@gmail.com>
2019-01-09 14:10:25 +1100

    fixed login on new keycloak registration page

--- src/spec/library/login/index.js
+++ src/spec/library/login/index.js
@@ -6,31 +6,34 @@ setDefaultOptions({ timeout: 25 * SECONDS });
 
 
 class Login {
-  run( page ) {
-    return new Promise( async ( resolve, reject ) => {
-      try {        
+  run(page) {
+    return new Promise(async (resolve, reject) => {
+      try {
         // Go to project URL
-        await page.goto( process.env.GENNY_URL );
+        await page.goto(process.env.GENNY_URL);
 
         // Click the Log in button
-        await expect( page ).toClick( '[data-testid="button"]' );
+        await expect(page).toClick('[data-testid="button"]');
+
+        await page.waitFor(2 * SECONDS);
 
         // Fill in user email and password
-        await expect( page ).toFill(
-          'input[type="text"]',
-          process.env.AGENT_A_USERNAME
+        await expect(page).toFill(
+          'input[name="username"]',
+          process.env.AGENT_A_USERNAME,
         );
-        await expect( page ).toFill(
-          'input[type="password"]',
-          process.env.AGENT_A_PASSWORD
+
+        await expect(page).toFill(
+          'input[name="password"]',
+          process.env.AGENT_A_PASSWORD,
         );
 
         // click the submit button
-        await expect( page ).toClick( 'div[data-testid="form-generic-submit"]' );
+        await expect(page).toClick('button[type="submit"]');
 
-        resolve( true );
-      } catch ( error ) {
-        reject( error );
+        resolve(true);
+      } catch (error) {
+        reject(error);
       }
     });
   }
