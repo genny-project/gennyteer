8c17b1117401aeedf0381839e7a5d9a09e976183
Alwy Christyo <alwychristyo@gmail.com>
2019-01-14 12:11:27 +1100

    updated eslint and stylings

--- src/spec/library/login/index.js
+++ src/spec/library/login/index.js
@@ -5,32 +5,32 @@ import { setDefaultOptions } from 'expect-puppeteer';
 setDefaultOptions({ timeout: 25 * SECONDS });
 
 class Login {
-  static run(page) {
-    return new Promise(async (resolve, reject) => {
+  static run( page ) {
+    return new Promise( async ( resolve, reject ) => {
       try {
         // Click the Log in button
-        await expect(page).toClick('[data-testid="button"]');
+        await expect( page ).toClick( '[data-testid="button"]' );
 
         await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
         // Fill in user email and password
-        await expect(page).toFill(
+        await expect( page ).toFill(
           '.input-email input',
           process.env.AGENT_A_USERNAME,
         );
-        await expect(page).toFill(
+        await expect( page ).toFill(
           'input[type="password"]',
           process.env.AGENT_A_PASSWORD,
         );
 
         // click the submit button
-        await expect(page).toClick('button[type="submit"]');
+        await expect( page ).toClick( 'button[type="submit"]' );
 
         await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-        resolve(true);
-      } catch (error) {
-        reject(error);
+        resolve( true );
+      } catch ( error ) {
+        reject( error );
       }
     });
   }
