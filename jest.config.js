d5cf4f4bec04d42efdcf121236e8a5d88ee1ad35
c7yrus <c7yrus@gmail.com>
2019-01-15 15:34:49 +1100

    changed the config file to default

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   rootDir: 'dist',
-  testMatch: ['<rootDir>/**/tests/10/10_smoke-test.js'],
+  testMatch: ['<rootDir>/**/tests/**/*.js','**/?(*.)(spec|test).js'],
   preset: 'jest-puppeteer'
 };
