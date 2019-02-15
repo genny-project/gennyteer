b839d2d0a88deaee8fbc254359dbf91c5308a998
Nischal Gautam <nischal1024@gmail.com>
2019-02-15 14:28:33 +1100

    change

--- src/interface/config.js
+++ src/interface/config.js
@@ -1,3 +1,8 @@
+import dotEnv from 'dotenv';
+import path from 'path';
+dotEnv.load();
+dotEnv.config({ path: path.resolve( __dirname, '/.env' ) });
+
 const config = {
   databse: {
     baseUrl: 'https://api-internmatch-staging.outcome-hub.com/api-docs/',
@@ -6,6 +11,15 @@ const config = {
     getQuestions: '',
     getAttributes: '',
     getLinksCodeByCode: ''
+  },
+  auth: {
+    port: process.env.PORT || 3000,
+    grant_type: process.env.GRANT_TYPE,
+    client_id: process.env.CLIENT_ID,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    client_secret: process.env.CLIENT_SECRET,
+    tokenUrl: process.env.TOKEN_URL
   }
 };
 
---
+++ src/interface/utils.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import queryString from 'querystring';
+import config from '../config';
+import * as constants from '../constants';
+
+/* get URLS */
+const { GET_TOKEN_URL } = constants;
+const tokenURL = GET_TOKEN_URL;
+
+/* request parameters to be sent */
+const requsetParams = {
+  grant_type: config.grant_type,
+  client_id: config.client_id,
+  username: config.username,
+  password: config.password,
+  client_secret: config.client_secret
+};
+
+// get token from Genny before calling the API
+const getTokenUtils = async () => {
+  console.debug( 'Testing for console debug ** reached get token **' );
+  try {
+    const response = await axios.post(
+      tokenURL,
+      queryString.stringify( requsetParams )
+    );
+    return response.data;
+  } catch ( err ) {
+    console.log( err );
+  }
+};
+
+export default getTokenUtils;
+
+/* token now coming through */
