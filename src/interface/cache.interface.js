e7e4f9d3efe6d865bd0ef858acb8f7f8e6f47ac3
Nischal Gautam <nischal1024@gmail.com>
2019-02-18 15:16:17 +1100

    cache interface working

--- src/interface/cache.interface.js
+++ src/interface/cache.interface.js
@@ -1,17 +1,39 @@
 // import config from './config';
 // import Api from './api';
 import axios from 'axios';
+
 class Cache {
-  static async checkIfBaseEntityExists() {
-    const resp = await axios.get(
-      'https://app3-internmatch-staging.outcome-hub.com/read/PRJ_INTERNMATCH'
-    );
+  static async checkIfBaseEntityExists(
+    baseEntity = 'PER_AGENT3_AT_GMAILCOM',
+    attributeName = 'PRI_FIRSTNAME',
+    valueKey = 'valueString',
+    expectedValue = 'Agent3'
+  ) {
+    let resp;
+    try {
+      resp = await axios.get(
+        `https://app3-internmatch-staging.outcome-hub.com/read/${baseEntity}`
+      );
+    } catch ( err ) {
+      throw new Error( ' Error getting the data from Cache' );
+    }
 
-    // console.log( resp.data.value );
+    console.log( resp.data );
+
     const jsonify = JSON.parse( resp.data.value );
-    console.log( jsonify );
+    const a = jsonify.baseEntityAttributes.find(
+      el => el.attributeCode === attributeName
+    );
+    const value = a[valueKey];
+    console.log( a );
+    console.log({ valueKey });
+    console.log({ value });
 
-    return resp;
+    if ( value === expectedValue ) {
+      return true;
+    } else {
+      throw new Error( ' expected value doesnot equals the value in the cache' );
+    }
   }
 }
 
--- src/interface/festore.interface.js
+++ src/interface/festore.interface.js
@@ -1,33 +1,33 @@
-// import storeJSON from './storeTest.json';
-// import deleve from 'dlv';
-// class FeStore {
-//   static baseEntityExists({ store = storeJSON, baseEntityCode }) {
-//     if ( store == null || store === 'undefined' ) {
-//       return false;
-//     } else {
-//       // check for baseEntitys get baseEntitys
-//       console.log({ store });
-//       const getKeysOfData = Object.keys(
-//         store.keycloak.vertex.baseEntities.data
-//       );
-//       const x = getKeysOfData.filter( aa => aa === baseEntityCode );
+import storeJSON from './storeTest.json';
+import deleve from 'dlv';
+class FeStore {
+  static baseEntityExists({ store = storeJSON, baseEntityCode }) {
+    if ( store == null || store === 'undefined' ) {
+      return false;
+    } else {
+      // check for baseEntitys get baseEntitys
+      console.log({ store });
+      const getKeysOfData = Object.keys(
+        store.keycloak.vertex.baseEntities.data
+      );
+      const x = getKeysOfData.filter( aa => aa === baseEntityCode );
 
-//       if ( x[0] === baseEntityCode ) {
-//         return true;
-//       }
-//       return false;
-//     }
-//   }
+      if ( x[0] === baseEntityCode ) {
+        return true;
+      }
+      return false;
+    }
+  }
 
-//   static attributeValueExists({ bsCode, attributeName, valueKey }) {
-//     const val = deleve(
-//       storeJSON,
-//       `keycloak.vertex.baseEntities.attributes.${bsCode}.${attributeName}.${
-//         valueKey ? valueKey : 'value'
-//       }`
-//     );
-//     return val;
-//   }
-// }
+  static attributeValueExists({ bsCode, attributeName, valueKey }) {
+    const val = deleve(
+      storeJSON,
+      `keycloak.vertex.baseEntities.attributes.${bsCode}.${attributeName}.${
+        valueKey ? valueKey : 'value'
+      }`
+    );
+    return val;
+  }
+}
 
-// export default FeStore;
+export default FeStore;
