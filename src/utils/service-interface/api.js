b1e13b94da9e309a2771714aadb2d00f571963a4
Nischal Gautam <nischal1024@gmail.com>
2019-04-23 14:42:13 +1000

    got api wrapper working

--- src/utils/service-interface/api.js
+++ src/utils/service-interface/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import querystring from 'querystring';
 
+// :String
 const filterMethods = method => {
   if ( !method ) return 'GET';
   const methods = {
@@ -10,16 +11,22 @@ const filterMethods = method => {
     put: 'PUT',
     patch: 'PATCH'
   };
+
+  // convert the incoming string to lowercases
+  // "GET" => "get" , "POST" => "post"
+
   const lowerMethod = method.toLowerCase();
   console.log( lowerMethod );
 
   if ( methods[lowerMethod] ) return methods[lowerMethod];
   throw Error( ' Request Method not valid' );
 };
+
 class Api {
+  // :<Promise> dynamic
   static async call({
     method,
-    url,
+    URL,
     params,
     accessToken,
     stringifyData = false
@@ -32,11 +39,10 @@ class Api {
     const stringifiedParams = stringifyData
       ? querystring.stringify( params )
       : params;
-
     try {
       const response = await axios({
-        method,
-        url: url,
+        method: method || 'GET',
+        url: URL,
         headers: { Authorization: `Bearer ${accessToken}` },
         data: stringifiedParams
       });
@@ -49,3 +55,8 @@ class Api {
 }
 
 export default Api;
+
+// Example
+// Api.call({ method: 'GET', URL: 'google.com' }).then( data => {
+//   console.log( data );
+// });
