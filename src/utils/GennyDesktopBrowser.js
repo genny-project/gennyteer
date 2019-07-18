d16b61b26eb723e3ed45a8da5eb632bad01a7470
siddardha kolanupaka <siddardhakolanupaka@siddardhas-MacBook-Pro.local>
2019-07-18 16:46:59 +1000

    slowMO using dockerfile

--- src/utils/GennyDesktopBrowser.js
+++ src/utils/GennyDesktopBrowser.js
@@ -68,11 +68,12 @@ class GennyDesktopBrowser {
 
   static async generatePage() {
     let browser = null;
+    const slowMo = process.env.SLOW_MO ?  process.env.SLOW_MO  : null;
     const args = ['--no-sandbox', `--window-size=${PAGE_WIDTH},${PAGE_HEIGHT}`];
     browser = await puppeteer.launch({
       args,
-      headless: false,
-      slowMo:120
+      headless:false,
+      slowMo:slowMo
     });
 
     const page = await browser.newPage();
@@ -86,9 +87,10 @@ class GennyDesktopBrowser {
   }
 
   //Screenshot this has not been tested yet
-  async screenshot( fileName ) {
-    const scShot = new ScSchot( this.page, fileName );
-    scShot.shoot();
+  async screenshot( fileName,screenshot_path ) {
+    console.log(screenshot_path);
+    const scShot = new ScSchot( this.page, fileName);
+    scShot.shoot(screenshot_path);
   }
 
   async clickOnSelector( selector ) {
