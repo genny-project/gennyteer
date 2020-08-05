2605b227dfb5cd665390d98b90bf04a0b3b0eb9c
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2020-08-05 16:20:15 +1000

    token change

--- src/utils/service-interface/asyncTokenUtils.js
+++ src/utils/service-interface/asyncTokenUtils.js
@@ -1,20 +1,15 @@
 import axios from 'axios';
 import queryString from 'querystring';
-
 /* Get URLS */
 const GET_TOKEN_URL = 'https://keycloak.gada.io/auth/realms/internmatch/protocol/openid-connect/token';
-
 const tokenURL = GET_TOKEN_URL;
-
 /* request parameters to be sent */
 const requsetParams = {
   grant_type: 'password',
-  client_id: 'internmatch',
+  client_id: 'alyson',
   username: 'user1',
   password: 'WelcomeToTheHub121!',
-  client_secret: 'dc7d0960-2e1d-4a78-9eef-77678066dbd3'
 };
-
 const getTokenUtils = async () => {
   // console.debug( 'Testing for console debug ** reached get token **' );
   try {
@@ -28,5 +23,4 @@ const getTokenUtils = async () => {
     console.log( err );
   }
 };
-
 export default getTokenUtils;
